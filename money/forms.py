17f7c6854e9a8b8ef91335cfb116379b20166c9f
MssIwng <locke.vis.2357@gmail.com>
2019-05-20 15:34:05 +0900

    forms.pyとmodels.pyの変更
    formをModelFormから作成。それに伴ってmodels.pyを変更。

--- money/forms.py
+++ money/forms.py
@@ -1,35 +1,16 @@
 from django import forms
-
 from .models import Money
 
-# フォームのクラスを作成する
-class SpendingForm(forms.Form):
-    #　各フォームの性質を定義。フィールド定義とラベル付けを行う
-    choices = (
-            ('家賃','家賃'),
-            ('通信費','通信費'),
-            ('保険費','保険費'),
-            ('クレカ','クレカ'),
-            ('ガソリン代','ガソリン代'),
-            ('雑費','雑費'),
-            ('光熱費','光熱費'),
-            ('車代','車代'),
-            ('奨学金','奨学金'),
-            ('ローン','ローン'),
-            ('食費','食費'),
-            ('交遊費','交遊費'),
-            ('NHK受信料','NHK受信料'),
-            ('その他','その他'),
-            )
-    amount = forms.IntegerField(label='金額')
+
+class SpendingForm(forms.ModelForm):
     exchange_date = forms.DateTimeField(
-            label='日付',
-            required=True,
-            widget=forms.DateInput(attrs={"type":"date"}),
-            input_formats=['%Y-%m-%d']
-            )
-    detail = forms.CharField(
-            max_length = 200,
-            label='用途'
-            )
-    category = forms.ChoiceField(choices=choices,label='カテゴリー')
+        label='日付',
+        required=True,
+        widget=forms.DateInput(attrs={"type": "date"}),
+        input_formats=['%Y-%m-%d']
+    )
+    # https://docs.djangoproject.com/ja/2.1/topics/forms/modelforms/#overriding-the-default-fields
+
+    class Meta:
+        model = Money
+        fields = ['amount', 'exchange_date', 'detail', 'category']
--- money/models.py
+++ money/models.py
@@ -3,15 +3,31 @@ from django.db import models
 """
 Djangoのモデルは、Modelクラスの継承クラスとして定義される
 """
+SELECT_CHOICES = (
+    ('家賃', '家賃'),
+    ('通信費', '通信費'),
+    ('保険費', '保険費'),
+    ('クレカ', 'クレカ'),
+    ('ガソリン代', 'ガソリン代'),
+    ('雑費', '雑費'),
+    ('光熱費', '光熱費'),
+    ('車代', '車代'),
+    ('奨学金', '奨学金'),
+    ('ローン', 'ローン'),
+    ('食費', '食費'),
+    ('交遊費', '交遊費'),
+    ('NHK受信料', 'NHK受信料'),
+    ('その他', 'その他'),
+)
 
 
 # Create your models here.
 class Money(models.Model):
     # お金をやり取りした日付、収支の内訳、金額を定義
     exchange_date = models.DateTimeField('日付')
-    detail = models.CharField(max_length=200)
-    amount = models.IntegerField(default=0)
-    category = models.CharField(max_length=10)
+    detail = models.CharField('用途', max_length=200)
+    amount = models.IntegerField('金額', default=0)
+    category = models.CharField('カテゴリー', max_length=10, choices=SELECT_CHOICES)
 
     def __str__(self):
         return self.detail + '￥' + str(self.amount)
